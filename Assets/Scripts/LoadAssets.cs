9309f2eb186fd7fa7c53b1bf3fbe28a45d2e3cf1
melikeo <98155324+melikeo@users.noreply.github.com>
2024-08-31 23:06:57 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,11 +6,20 @@ public class LoadAssets : MonoBehaviour
 
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Limit framerate to 60fps
+        Application.targetFrameRate = 60;
+
+        //creates redobj
+        Instantiate(redObj, new Vector3 (2, 0, 0), Quaternion.identity);
+        //creates blueobj
+        Instantiate(blueObj, new Vector3 (-2, 0, 0), Quaternion.identity);
+
     }
 
     // Update is called once per frame
