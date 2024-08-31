ae74af36b34acf788382cff710d6df7533270b9e
melikeo <98155324+melikeo@users.noreply.github.com>
2024-09-01 00:46:19 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 
 {
     public Renderer rend;
+    private int i = 3;
+    private int randomValue;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomValue = Random.Range(150, 251);
+                
     }
 
     // Update is called once per frame
-    int i = 3;
-    void Update()
+        void Update()
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100) 
+        {
+            gameObject.SetActive(false);
+        };
+
+        if (gameObject.tag == "Blue" && i == randomValue) 
+        {
+            rend.enabled = false;
+     
+        };
         
     }
 }
